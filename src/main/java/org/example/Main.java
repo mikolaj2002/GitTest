2b247de3fbd0be66b613623dede57a3ce5c4b706
mikolaj_krupinski <mikolaj_krupinski@wp.pl>
2022-10-21 09:33:51 +0200

    commit from test1 branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main
 {
     public static void main(String[] args) {
-        System.out.println("Message from test1 branch");
+        System.out.println("Message");
     }
 }
